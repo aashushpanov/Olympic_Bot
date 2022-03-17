4b9443239242fd29c6aaf15febb1b0d18fa53db7
Alexey <aashushpanov>
2022-03-17 22:52:30 +0300

    make structure

---
+++ bot.py
@@ -0,0 +1,12 @@
+
+
+async def on_startup(dp):
+    pass
+
+
+if __name__ == '__main__':
+    from aiogram import executor
+    from handlers import dp
+
+    executor.start_polling(dp, on_startup=on_startup)
+
